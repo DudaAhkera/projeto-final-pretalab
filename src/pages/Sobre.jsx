e43b70e1b19e829f021c03207f8350d3df0f9e8d
DudaAhkera <rochapereira16@gmail.com>
2022-12-09 17:41:26 -0300

    Add primeira imagem na page Sobre

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,12 +1,24 @@
 import { Header } from "../components/Header"
 import homeImg from '../assets/about.svg'
+import Styles from "../Styles/Pages/Sobre.module.css"
 
 function Sobre() {
     return (
         <>
-            <Header text="Como tudo comecou..." image={homeImg}/>
+            <Header text="Como tudo comecou..." image={homeImg} />
+            <div className={Styles.squareContainer}>
+                <p className={Styles.textCard}>Iniciei os estudos há mais ou menos 1 ano. 
+                    Morei dois anos e meio na Finlândia. Meu marido foi à trabalho. 
+                    Lá é uma vida muito diferente, é um país muito bem estruturado e que investe muito bem em educacão e tecnologia e por isso 
+                    fiz meu primeiro curso de tecnologia, o Machine Learning. Fiquei muito empolgada com tudo que aprendi e relembrei meu "amor platônico", 
+                    por isso resolvi correr atrás do tempo perdido.
+                </p>
+                <img src="./src/assets/Museokeskus.jpg" alt="Foto de uma mouse e um teclado antigo tirada em um museu" className={Styles.imgCard}/>
+                
+                
+
+            </div>
             
-            <h1>Iniciei os estudos há pouco menos de um ano. Morei durante dois anos e meio na Finlândia. Meu marido foi à trabalho e lá é uma vida muito diferente e por ser um país muito bem estruturado e investir muito bem em educacão e tecnologia, fiz meu primeiro curso de tecnologia, o Machine Learning. Fiquei muito empolgada com tudo que aprendi e lembrei que a tecnologia já havia sido um "amor platônico", por isso resolvi correr atrás do tempo perdido.</h1>
         </>
     )
 }
