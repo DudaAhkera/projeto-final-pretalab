5164670190a9c8aefadf61a84c13975eab5657aa
DudaAhkera <rochapereira16@gmail.com>
2022-12-12 17:14:58 -0300

    Finalizacão page sobre e estilizacão.

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -11,12 +11,12 @@ function Inicio () {
                 <div className={Styles.cardContainer}>
                     <Alien size={150} color="#686AAC" weight="thin" />
                     <h2 className={Styles.cardTitle}>Vida</h2>
-                    <p className={Styles.cardText}>Eu conto um pouquinho sobre mim, minha formação, a história da minha migração de carreira, gostos e curiosidades. </p>
+                    <p className={Styles.cardText}>Eu conto um pouquinho sobre mim, minha formação, a história da minha migração de carreira. </p>
                 </div>
                 <div className={Styles.cardContainer}>
                     <BracketsCurly size={150} color="#686AAC" weight="thin" />
                     <h2 className={Styles.cardTitle}>Código</h2>
-                    <p className={Styles.cardText}>Mostro meu portfólio. Tudo o que aprendi até o momento dando destaque para alguns dos projetos que criei enquanto estudo programação. </p>
+                    <p className={Styles.cardText}>Mostro meu portfólio. O que aprendi até o momento enquanto estudo programação. </p>
                 </div>
                 <div className={Styles.cardContainer}>
                     <AddressBook size={150} color="#686AAC" weight="thin" />
--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -7,18 +7,37 @@ function Sobre() {
         <>
             <Header text="Como tudo comecou..." image={homeImg} />
             <div className={Styles.squareContainer}>
-                <p className={Styles.textCard}>Iniciei os estudos há mais ou menos 1 ano. 
-                    Morei dois anos e meio na Finlândia. Meu marido foi à trabalho. 
-                    Lá é uma vida muito diferente, é um país muito bem estruturado e que investe muito bem em educacão e tecnologia e por isso 
-                    fiz meu primeiro curso de tecnologia, o Machine Learning. Fiquei muito empolgada com tudo que aprendi e relembrei meu "amor platônico", 
-                    por isso resolvi correr atrás do tempo perdido.
-                </p>
-                <img src="./src/assets/Museokeskus.jpg" alt="Foto de uma mouse e um teclado antigo tirada em um museu" className={Styles.imgCard}/>
                 
+                <div className={Styles.Card}>
+                    <p className={Styles.textCard}>
+                        Foi fascinante fazer meu primeiro curso de informática em um projeto governamental. 
+                        E posteriormente os cursos de informática que meu pai pagou, a grande felicidade de ter meu primeiro computador, 
+                        cada fórmula no excel que aprendi (era um grito de alegria)...
+                    </p>
+                    <a href=""><img src="./src/assets/excel.png" alt="Símbolo do microsoft excel" className={Styles.imgCard} /></a>
+                </div>
                 
+                <div className={Styles.Card}>
+                    <p className={Styles.textCard}>
+                        Sou administradora de formacão, passeei pelo empreendedorismo mas tenho experiência com administrativo, atendimento ao cliente e vendas. 
+                        Me dediquei a estudar redes sociais, experiência do usuário, marketing. 
+                        Sou certificada gerente de projetos...
+                    </p>
+                    <a href="https://openbadgefactory.com/v1/assertion/dc3d4c6139c0a23916fb4985eea83b1ce98c6be9.html" ><img src="./src/assets/sertifikaatti.jpg" alt="Certificado do curso de Gerenciamento de Projetos" className={Styles.imgCard} /></a>
+                </div>
+                <div className={Styles.Card}>
+                    <p className={Styles.textCard}>
+                        Retomei os estudos na área de tecnologia há mais de 1 ano.
+                        Morei dois anos e meio na Finlândia.
+                        Lá é uma vida muito diferente, é um país muito bem estruturado e que investe muito bem em educacão e tecnologia e por isso
+                        fiz meu primeiro curso, o Machine Learning. Fiquei muito empolgada com tudo que aprendi e relembrei meu "amor platônico",
+                        por isso resolvi correr atrás do tempo perdido!
+                    </p>
+                    <a href="https://www.vapriikki.fi/en/" ><img src="./src/assets/Museokeskus.jpg" alt="Foto tirada em um museu de um mouse e teclado antigos" className={Styles.imgCard} /></a>
+                </div>
 
             </div>
-            
+
         </>
     )
 }
