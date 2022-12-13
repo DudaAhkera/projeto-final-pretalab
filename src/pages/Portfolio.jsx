e69682a49e919806ff6fa2744cdfbff6d20c3bf2
DudaAhkera <rochapereira16@gmail.com>
2022-12-13 09:48:30 -0300

    src={imagevariable}

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -15,21 +15,21 @@ function Portfolio () {
                 <div className={Styles.cardContainer}>
 
                     <h2>Currículo</h2>
-                    <img src="./src/assets/curriculo.png" alt="" className={Styles.cardImg} image={menu1Img}/>
+                    <img src={menu1Img} alt="" className={Styles.cardImg} />
                     <a href="https://github.com/DudaAhkera/resume" className={Styles.a}>Meu currículo </a>
 
                 </div>
                 <div className={Styles.cardContainer}>
 
                     <h2>Jogo do Mário</h2>
-                    <img src="./src/assets/game.png" alt="" className={Styles.cardImg} image={menu2Img}/>
+                    <img src={menu2Img} alt="" className={Styles.cardImg} />
                     <a href="https://github.com/DudaAhkera/Aula_jsbasico_startse_jogo_mario/tree/main" className={Styles.a}> Vamos jogar Mário?</a>
                     
                 </div>
                 <div className={Styles.cardContainer}>
 
                     <h2>Qual a sua idade? </h2>
-                    <img src="./src/assets/tempo.png" alt="" className={Styles.cardImg} image={menu3Img}/>
+                    <img src={menu3Img} alt="" className={Styles.cardImg} />
                     <a href="https://github.com/DudaAhkera/Qual_a_sua_idade_treino_js" className={Styles.a}>Fases da vida</a>
                     
                 </div>
--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -17,7 +17,7 @@ function Sobre() {
                         E posteriormente os cursos de informática que meu pai pagou, a grande felicidade de ter meu primeiro computador, 
                         cada fórmula no excel que aprendi (era um grito de alegria)...
                     </p>
-                    <a href=""><img src="./src/assets/excel.png" alt="Símbolo do microsoft excel" className={Styles.imgCard} image={card1Img} /></a>
+                    <a href=""><img src={card1Img}  alt="Símbolo do microsoft excel" className={Styles.imgCard} /></a>
                 </div>
                 
                 <div className={Styles.Card}>
@@ -26,7 +26,7 @@ function Sobre() {
                         Me dediquei a estudar redes sociais, experiência do usuário, marketing. 
                         Sou certificada gerente de projetos...
                     </p>
-                    <a href="https://openbadgefactory.com/v1/assertion/dc3d4c6139c0a23916fb4985eea83b1ce98c6be9.html" ><img src="./src/assets/sertifikaatti.jpg" alt="Certificado do curso de Gerenciamento de Projetos" className={Styles.imgCard} image={card2Img} /></a>
+                    <a href="https://openbadgefactory.com/v1/assertion/dc3d4c6139c0a23916fb4985eea83b1ce98c6be9.html" ><img src={card2Img} alt="Certificado do curso de Gerenciamento de Projetos" className={Styles.imgCard}  /></a>
                 </div>
                 <div className={Styles.Card}>
                     <p className={Styles.textCard}>
@@ -36,7 +36,7 @@ function Sobre() {
                         fiz meu primeiro curso, o Machine Learning. Fiquei muito empolgada com tudo que aprendi e relembrei meu "amor platônico",
                         por isso resolvi correr atrás do tempo perdido!
                     </p>
-                    <a href="https://www.vapriikki.fi/en/" ><img src="./src/assets/Museokeskus.jpg" alt="Foto tirada em um museu de um mouse e teclado antigos" className={Styles.imgCard} image={card3Img}/></a>
+                    <a href="https://www.vapriikki.fi/en/" ><img src={card3Img} alt="Foto tirada em um museu de um mouse e teclado antigos" className={Styles.imgCard} /></a>
                 </div>
 
             </div>
