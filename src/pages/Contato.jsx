ba6cddb5833d7ed9588392574316120ca5a33e22
DudaAhkera <rochapereira16@gmail.com>
2022-12-12 23:12:27 -0300

    finalizacão contato.module.css, inclusão icones

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -4,6 +4,7 @@ import contatoImg from '../assets/contact.svg'
 import Styles from '../Styles/Pages/Contato.module.css'
 import { database } from '../services/firebase'
 import { ref, push, set } from 'firebase/database'
+import { InstagramLogo, GithubLogo, LinkedinLogo } from 'phosphor-react'
 
 export function Contato () {
     const [nome, setNome] = useState('')
@@ -63,6 +64,28 @@ export function Contato () {
                     <textarea onChange={handleInputMessage} placeholder="Digite sua mensagem" className={Styles.formTextArea} value={message}></textarea>
                     <button className={Styles.formButton} type="submit">Enviar Mensagem</button>
                 </form>
+            </div>
+            <div className={Styles.containerFooter}>
+                <div className={Styles.listaFooter}>
+                    <div className={Styles.icon}>
+                        <a href="https://www.instagram.com/mira_ncl/" target="_blank" className={Styles.links}>
+                            <InstagramLogo size={32} color="#686AAC" weight="thin" />mira_ncl
+                        </a>
+                    </div>
+
+                    <div className={Styles.icon}>
+                        <a href="https://www.linkedin.com/in/zulenice-pereira-84535536/" target="_blank" className={Styles.links}>
+                            <LinkedinLogo size={32} color="#686AAC" weight="thin" />Zulenice Pereira
+                        </a>
+                    </div>
+
+                    <div className={Styles.icon}>
+                        <a href="https://github.com/DudaAhkera" target="_blank" className={Styles.links}>
+                            <GithubLogo size={32} color="#686AAC" weight="thin" />Duda Ahkera
+                        </a>
+                    </div>
+                </div>
+
             </div>
         </>
     )
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,11 +1,37 @@
 import { Header } from "../components/Header"
 import homeImg from '../assets/portfolio.svg'
+import Styles from "../Styles/Pages/Portfolio.module.css"
 
 function Portfolio () {
     return(
         <>
             <Header text="Meus Projetinhos fofos" image={homeImg}/>
-            <h1>Aqui estão os projetos criados durante os estudos no PretaLab</h1>
+            
+            <div className={Styles.homeContainer}>
+
+                <div className={Styles.cardContainer}>
+
+                    <h2>Currículo</h2>
+                    <img src="./src/assets/curriculo.png" alt="" className={Styles.cardImg} />
+                    <a href="https://github.com/DudaAhkera/resume" className={Styles.a}>Meu currículo </a>
+
+                </div>
+                <div className={Styles.cardContainer}>
+
+                    <h2>Jogo do Mário</h2>
+                    <img src="./src/assets/game.png" alt="" className={Styles.cardImg} />
+                    <a href="https://github.com/DudaAhkera/Aula_jsbasico_startse_jogo_mario/tree/main" className={Styles.a}> Vamos jogar Mário?</a>
+                    
+                </div>
+                <div className={Styles.cardContainer}>
+
+                    <h2>Qual a sua idade? </h2>
+                    <img src="./src/assets/tempo.png" alt="" className={Styles.cardImg} />
+                    <a href="https://github.com/DudaAhkera/Qual_a_sua_idade_treino_js" className={Styles.a}>Fases da vida</a>
+                    
+                </div>
+            </div>
+        
         </>
     )
 }
