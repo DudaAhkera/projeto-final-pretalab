4bd5d0499b502117fd38d03362b38b5a77fbb65a
DudaAhkera <rochapereira16@gmail.com>
2022-12-13 11:21:54 -0300

    styles com s minúsculo

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,27 +1,27 @@
 import { Header } from "../components/Header"
 import homeImg from '../assets/home.svg'
 import { Alien, BracketsCurly, AddressBook } from 'phosphor-react'
-import Styles from '../Styles/Pages/Inicio.module.css'
+import styles from '../Styles/Pages/Inicio.module.css'
 
 function Inicio () {
     return(
         <>
             <Header text="Meu site Pessoal" image={homeImg}/>
-            <div className={Styles.homeContainer}>
-                <div className={Styles.cardContainer}>
+            <div className={styles.homeContainer}>
+                <div className={styles.cardContainer}>
                     <Alien size={150} color="#686AAC" weight="thin" />
-                    <h2 className={Styles.cardTitle}>Vida</h2>
-                    <p className={Styles.cardText}>Eu conto um pouquinho sobre mim, minha formação, a história da minha migração de carreira. </p>
+                    <h2 className={styles.cardTitle}>Vida</h2>
+                    <p className={styles.cardText}>Eu conto um pouquinho sobre mim, minha formação, a história da minha migração de carreira. </p>
                 </div>
-                <div className={Styles.cardContainer}>
+                <div className={styles.cardContainer}>
                     <BracketsCurly size={150} color="#686AAC" weight="thin" />
-                    <h2 className={Styles.cardTitle}>Código</h2>
-                    <p className={Styles.cardText}>Mostro meu portfólio. O que aprendi até o momento enquanto estudo programação. </p>
+                    <h2 className={styles.cardTitle}>Código</h2>
+                    <p className={styles.cardText}>Mostro meu portfólio. O que aprendi até o momento enquanto estudo programação. </p>
                 </div>
-                <div className={Styles.cardContainer}>
+                <div className={styles.cardContainer}>
                     <AddressBook size={150} color="#686AAC" weight="thin" />
-                    <h2 className={Styles.cardTitle}> Contatinho</h2>
-                    <p className={Styles.cardText}>Entre em contato comigo. Seja para entrevistas de emprego como deva ou para trocarmos ideias sobre os estudos.</p>
+                    <h2 className={styles.cardTitle}> Contatinho</h2>
+                    <p className={styles.cardText}>Entre em contato comigo. Seja para entrevistas de emprego como deva ou para trocarmos ideias sobre os estudos.</p>
                 </div>
 
             </div>
