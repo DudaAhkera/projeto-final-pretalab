1b4533f2d0c095cb8ca5009487cbae62caca8782
DudaAhkera <rochapereira16@gmail.com>
2022-12-13 12:41:54 -0300

    copiei todo inicio da prof

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Inicio from "./pages/Inicio"
+import { Inicio } from "./pages/Inicio"
 import Contato from "./pages/Contato"
 import Portfolio from "./pages/Portfolio"
 import Sobre from "./pages/Sobre"
